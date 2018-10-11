b099f823c55f6d4e6d8885a76d40e75cbcba4601
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-11 09:20:25 -0700

    initial setup

---
+++ lab03-wordGuessGame/lab03-wordGuessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab03_wordGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
